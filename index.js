bda83a9720f7e1bedc0c5419ea85da9b61d498cb
John Goff <john.g@hiredhippo.ai>
2020-04-26 10:20:33 -0400

    listen for midi messages and send keypresses

--- index.js
+++ index.js
@@ -11,28 +11,61 @@ const apcout = new easymidi.Output("APC MINI");
 
 // Configure keybindings
 const bindings = {
-  56: "u",	  // hardpoints
-  48: "v",	  // Heatsink
-  32: "m",	  // Mode switch
-  24: "'",	  // FSS
-  8:  "r",	  // Thrust up
-  0:  "f",	  // Thrust down
-  57: "g",	  // Next Target
-  58: "1",	  // Contacts
-  59: "2",	  // Comms
-  35: "l",	  // lights
+  56: "u",    // hardpoints
+  48: "v",    // Heatsink
+  32: "m",    // Mode switch
+  24: "'",    // FSS
+  8:  "r",    // Thrust up
+  0:  "f",    // Thrust down
+  57: "g",    // Next Target
+  58: "1",    // Contacts
+  59: "2",    // Comms
+  35: "l",    // lights
   27: "Home", // Cargo scoop
   60: "3",    // Helm
-  36: "c",	  // Night Vision
-  28: "/",	  // Landing gear
-  61: "4"		  // Ship
-  62: ";",	  // 75% throttle
-  63: "j",	  // Frame Shift Drive
-  55: "k",	  // Supercruise
-  47: ",",	  // Next system
-  39: "b",	  // Galaxy Map
-  31: "]"		  // System Map
+  36: "c",    // Night Vision
+  28: "/",    // Landing gear
+  61: "4",    // Ship
+  62: ";",    // 75% throttle
+  63: "j",    // Frame Shift Drive
+  55: "k",    // Supercruise
+  47: ",",    // Next system
+  39: "b",    // Galaxy Map
+  31: "]"     // System Map
 }
+
+function setLights(button, colour) {
+  apcout.send("noteon", { note: button, velocity: colour });
+}
+
+function resetLightsToStarter() {
+  setLights(56, 03);
+  setLights(48, 05);
+  setLights(32, 03);
+  setLights(24, 05);
+  setLights(08, 03);
+  setLights(00, 03);
+  setLights(57, 01);
+  setLights(58, 05);
+  setLights(59, 05);
+  setLights(35, 05);
+  setLights(27, 01);
+  setLights(60, 05);
+  setLights(36, 01);
+  setLights(28, 05);
+  setLights(61, 05);
+  setLights(62, 01);
+  setLights(63, 01);
+  setLights(55, 01);
+  setLights(47, 05);
+  setLights(39, 01);
+  setLights(31, 01);
+}
+
+// Send keypresses when we get a MIDI message
+apcin.on("noteon", (msg) => {
+  robot.keyTap(bindings[msg.note]);
+});
 
 // Journal Server connection
 const ws = new WebSocket('ws://localhost:31337');
