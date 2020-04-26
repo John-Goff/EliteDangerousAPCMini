a0cfccaf90cbe047d2d3fec3027027b529b1d7d4
John Goff <john.g@hiredhippo.ai>
2020-04-26 17:16:37 -0400

    set lights based on payload

--- lights.js
+++ lights.js
@@ -11,6 +11,43 @@ const RED_FLASH = 4;
 const YELLOW = 5;
 const YELLOW_FLASH = 6;
 
+// Flags
+const ED_Docked             = 0x0000000000000001
+const ED_Landed             = 0x0000000000000002
+const ED_LandingGearDown    = 0x0000000000000004
+const ED_ShieldsUp          = 0x0000000000000008
+const ED_Supercruise        = 0x0000000000000010
+const ED_FlightAssistOff    = 0x0000000000000020
+const ED_HardpointsDeployed = 0x0000000000000040
+const ED_InWing             = 0x0000000000000080
+
+const ED_LightsOn           = 0x0000000000000100
+const ED_CargoScoopDeployed = 0x0000000000000200
+const ED_SilentRunning      = 0x0000000000000400
+const ED_ScoopingFuel       = 0x0000000000000800
+const ED_SRVHandbrake       = 0x0000000000001000
+const ED_SRVTurret          = 0x0000000000002000
+const ED_SRVTurretRetracted = 0x0000000000004000
+const ED_SRVDriveAssist     = 0x0000000000008000
+
+const ED_FSDMassLocked      = 0x0000000000010000
+const ED_FSDCharging        = 0x0000000000020000
+const ED_FSDCooldown        = 0x0000000000040000
+const ED_LowFuel            = 0x0000000000080000
+const ED_OverHeating        = 0x0000000000100000
+const ED_HasLatLong         = 0x0000000000200000
+const ED_IsInDanger         = 0x0000000000400000
+const ED_BeingInterdicted   = 0x0000000000800000
+
+const ED_InMainShip         = 0x0000000001000000
+const ED_InFighter          = 0x0000000002000000
+const ED_InSRV              = 0x0000000004000000
+const ED_HudInAnalysisMode  = 0x0000000008000000
+const ED_NightVision        = 0x0000000010000000
+
+// Masks
+const ED_CantFSD = ED_LandingGearDown | ED_HardpointsDeployed | ED_CargoScoopDeployed | ED_FSDMassLocked | ED_FSDCooldown | ED_InFighter | ED_InSRV;
+
 function setLights(midiout, button, colour) {
   midiout.send("noteon", { note: button, velocity: colour });
 }
@@ -50,6 +87,10 @@ function updateLights(midiout, payload) {
     setPipLights(midiout, eng, 14)
     setPipLights(midiout, wep, 07)
   }
+
+  setFocusLights(midiout, payload.GuiFocus || 0);
+
+  setFlagLights(midiout, payload.Flags);
 }
 
 function setPipLights(midiout, pips, id) {
@@ -59,6 +100,83 @@ function setPipLights(midiout, pips, id) {
     setLights(midiout, id, YELLOW);
   } else {
     setLights(midiout, id, GREEN);
+  }
+}
+
+function setFocusLights(midiout, focus) {
+  resetFocusLights(midiout);
+  if (focus === 1) {
+    setLights(midiout, 61, GREEN);
+  } else if (focus === 2) {
+    setLights(midiout, 58, GREEN);
+  } else if (focus === 3) {
+    setLights(midiout, 59, GREEN);
+  } else if (focus === 4) {
+    setLights(midiout, 60, GREEN);
+  } else if (focus === 6) {
+    setLights(midiout, 39, GREEN_FLASH);
+  } else if (focus === 7) {
+    setLights(midiout, 31, GREEN_FLASH);
+  } else if (focus === 9) {
+    setLights(midiout, 24, YELLOW_FLASH);
+  }
+}
+
+function resetFocusLights(midiout) {
+  setLights(midiout, 24, YELLOW); // FSS
+  setLights(midiout, 58, YELLOW); // contacts
+  setLights(midiout, 59, YELLOW); // comms
+  setLights(midiout, 60, YELLOW); // helm
+  setLights(midiout, 61, YELLOW); // ship
+  setLights(midiout, 39, GREEN);  // galaxy map
+  setLights(midiout, 31, GREEN);  // system map
+}
+
+function setFlagLights(midiout, flag) {
+  if (flag & ED_LandingGearDown) {
+    setLights(midiout, 28, RED);
+  } else {
+    setLights(midiout, 28, YELLOW);
+  }
+
+  if (flag & ED_LightsOn) {
+    setLights(midiout, 35, RED);
+  } else {
+    setLights(midiout, 35, YELLOW);
+  }
+
+  if (flag & ED_CargoScoopDeployed) {
+    setLights(midiout, 27, RED);
+  } else {
+    setLights(midiout, 27, GREEN);
+  }
+
+  if (flag & ED_NightVision) {
+    setLights(midiout, 36, RED);
+  } else {
+    setLights(midiout, 36, GREEN);
+  }
+
+  if (flag & ED_CantFSD) {
+    setLights(midiout, 63, RED);
+    setLights(midiout, 55, RED);
+  } else {
+    setLights(midiout, 63, GREEN);
+    setLights(midiout, 55, GREEN);
+  }
+
+  if (flag & ED_FSDCharging) {
+    setLights(midiout, 63, GREEN_FLASH);
+    setLights(midiout, 55, GREEN_FLASH);
+  } else {
+    setLights(midiout, 63, GREEN);
+    setLights(midiout, 55, GREEN);
+  }
+
+  if (flag & ED_HardpointsDeployed) {
+    setLights(midiout, 56, RED_FLASH);
+  } else {
+    setLights(midiout, 56, RED);
   }
 }
 
